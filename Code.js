38c257a794b67e126e3a26cb119ad61e312ea216
Janidu Wanigasuriya <janidu.wanigasuriya@rea-group.com>
2018-06-02 23:04:04 +1000

    Initial commit

---
+++ Code.js
@@ -0,0 +1,69 @@
+function getName() {
+  var email = Session.getActiveUser().getEmail();
+  var contact = ContactsApp.getContact(email);
+ 
+  var name = contact.getGivenName();
+  Logger.log('Name is' + name);
+
+  return name;
+}
+
+function preloadFormData() {
+  var name = getName();
+  
+  if (!name) {
+    name = name.getFullName();
+  } else {
+    Logger.log('Unable to load name');
+  }
+
+  var form = FormApp.getActiveForm();
+  
+  form.setTitle('Hello ' + name + ", please update your status");
+}
+
+function onOpen(e) {
+  Logger.log('onOpenForm');
+  preloadFormData();
+}
+
+// identifies the logged-in user, matches the row of the master spreadsheet
+// Email column must be 3rd column
+// spreadsheet id must not change
+function matchUserToRow() {
+  var email = Session.getActiveUser().getEmail();
+  var sheet = SpreadsheetApp.openById('1q92rCbdK0JHiQ_2dbBEp0uBcIVNxuzqzqw5fBYY0Ixs'); // id of 'Copy of In/out Board'
+  var data = sheet.getDataRange().getValues();
+  for(var i = 0; i<data.length;i++){
+    if(data[i][2] == email){ // [0] is column C
+      Logger.log((i));
+      return i + 1;
+    }
+  }
+}
+
+function onFormSubmit(e) {
+  // thank employee
+  var form = FormApp.getActiveForm();
+  var name = getName();
+  // form.setTitle(name + ", thanks for updating your status!");
+  
+  // get user-entered data from form submission
+  var formItems = form.getItems();
+  
+  // first form item is "status"
+  var status = e.response.getResponseForItem(formItems[0]).getResponse();
+  
+  // second form item is "notes"
+  var notes = e.response.getResponseForItem(formItems[1]).getResponse();
+    
+  // get user's row, and cells to update
+  var row = matchUserToRow();
+  var statusCell = 'E' + row; // status column must be E
+  var notesCell = 'F' + row;  // notes column must be F
+
+  // update cells
+  // assumes master spreadsheet id will not change! This is the id of 'Copy of In/out Board'
+  SpreadsheetApp.openById('1q92rCbdK0JHiQ_2dbBEp0uBcIVNxuzqzqw5fBYY0Ixs').getRange(statusCell).setValue(status);
+  SpreadsheetApp.openById('1q92rCbdK0JHiQ_2dbBEp0uBcIVNxuzqzqw5fBYY0Ixs').getRange(notesCell).setValue(notes);
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # family-life-app-scripts
 App scripts that drive family life forms
+
+# Install clasp
+
+`npm install @google/clasp -g`
+
+https://developers.google.com/apps-script/guides/clasp
+
+# 
